d7b9c3030745badf7753bcc6ddd579712e323b93
Viktar Marchuk <viktar_marchuk@epam.com>
2023-03-29 19:05:59 +0000

    Update core/pom.xml, api/pom.xml Deleted src/test/java/com/epam/tvmaze/api/SearchApiTests.java, src/test/java/com/epam/tvmaze/api/ShowApiTests.java, src/test/java/com/epam/tvmaze/api/UserDataApiTests.java, src/main/java/com/epam/tvmaze/api/http_api/DataApi.java, src/main/java/com/epam/tvmaze/api/apitest/SearchApi.java, src/main/java/com/epam/tvmaze/api/apitest/ShowApi.java, src/main/java/com/epam/tvmaze/api/apitest/UserDataApi.java, src/main/java/com/epam/tvmaze/api/apitest/EpisodeApi.java, src/main/java/com/epam/tvmaze/api/apitest/PeopleApi.java, src/test/java/com/epam/tvmaze/api/EpisodeApiTests.java, src/test/java/com/epam/tvmaze/api/PeopleApiTests.java

--- src/main/java/com/epam/tvmaze/api/http_api/DataApi.java
+++
@@ -1,9 +0,0 @@
-package com.epam.tvmaze.api.http_api;
-
-public class DataApi{
-    public final String BASE_URL = "https://api.tvmaze.com/";
-    public final String CORRECT_PERSON_NAME_ENDPOINT = "search/people?q=Sarah Shahi";
-    public final String INCORRECT_PERSON_NAME_ENDPOINT = "search/people?q=&";
-    public final String CORRECT_EPISODE_ENDPOINT = "shows/1/episodebynumber?season=1&number=1";
-    public final String INCORRECT_EPISODE_ENDPOINT = "1/episodebynumber?season=1&number=0";
-}
