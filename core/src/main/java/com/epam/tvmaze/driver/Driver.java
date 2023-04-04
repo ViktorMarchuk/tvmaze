11a054d28113bdce93d68a025e183c8078c991b9
Anastasiya Khinevich <xinevich.av@gmail.com>
2023-04-04 14:26:23 +0200

    add: remote driver

--- core/src/main/java/com/epam/tvmaze/driver/Driver.java
+++ core/src/main/java/com/epam/tvmaze/driver/Driver.java
@@ -2,10 +2,16 @@ package com.epam.tvmaze.driver;
 
 import com.epam.tvmaze.browser.WebDriverFactory;
 import com.epam.tvmaze.utils.ConfigEnum;
+import com.epam.tvmaze.utils.ConfigEnumDriverRemote;
 import com.epam.tvmaze.utils.ConfigReader;
 import io.github.bonigarcia.wdm.config.DriverManagerType;
+import io.github.bonigarcia.wdm.config.WebDriverManagerException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Objects;
 
 public class Driver {
@@ -16,7 +22,8 @@ public class Driver {
 
     public static synchronized WebDriver getInstance() {
         if (Objects.isNull(getThreadLocalDriver())) {
-            WebDriver driver = WebDriverFactory.installDriver(getValueOfBrowser());
+            boolean isRemote = Boolean.parseBoolean(ConfigReader.getValue(ConfigEnumDriverRemote.DRIVER_REMOTE));
+            WebDriver driver = isRemote ? createRemoteInstance() : WebDriverFactory.installDriver(getValueOfBrowser());
             threadLocalDriver.set(driver);
         }
         WebDriver driver = getThreadLocalDriver();
@@ -33,6 +40,25 @@ public class Driver {
         return Objects.isNull(browserSystem) ?
                 DriverManagerType.valueOf(ConfigReader.getValue(ConfigEnum.BROWSER).toUpperCase()) :
                 DriverManagerType.valueOf(browserSystem.toUpperCase());
+    }
+
+    private static WebDriver createRemoteInstance() {
+        String url = String.format("https://%s:%s%s",
+                ConfigReader.getValue(ConfigEnumDriverRemote.USER_NAME),
+                ConfigReader.getValue(ConfigEnumDriverRemote.ACCESS_KEY),
+                ConfigReader.getValue(ConfigEnumDriverRemote.LT_URL));
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", ConfigReader.getValue(ConfigEnumDriverRemote.BROWSER_NAME));
+        capabilities.setCapability("browserVersion", ConfigReader.getValue(ConfigEnumDriverRemote.BROWSER_VERSION));
+        capabilities.setCapability("platform", ConfigReader.getValue(ConfigEnumDriverRemote.PLATFORM));
+        capabilities.setCapability("buildName", ConfigReader.getValue(ConfigEnumDriverRemote.BUILD_NAME));
+
+        try {
+            return new RemoteWebDriver(new URL(url), capabilities);
+        } catch (MalformedURLException e) {
+            throw new WebDriverManagerException("RemoteWebDriver can not be created");
+        }
     }
 
     public static void closeDriver() {
