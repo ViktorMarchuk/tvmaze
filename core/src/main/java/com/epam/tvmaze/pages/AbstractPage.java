024794b091797ecda4b0a7c7a552bb41ff81c22f
Viktor Marchuk <viktor.m77.77@mail.ru>
2023-04-11 20:55:44 +0300

    EPMFARMATS-17255 Automate UI: Filter

--- core/src/main/java/com/epam/tvmaze/pages/AbstractPage.java
+++ core/src/main/java/com/epam/tvmaze/pages/AbstractPage.java
@@ -1,30 +1,15 @@
 package com.epam.tvmaze.pages;
 
 import com.epam.tvmaze.driver.Driver;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class AbstractPage {
-    protected WebDriver driver;
 
-    public AbstractPage() {
-        driver = Driver.getInstance();
-        PageFactory.initElements(driver, this);
-    }
+  protected WebDriver driver;
 
-    public List <String> getTextFromArrayXpath(String xpath){
-        List <WebElement> elements = driver.findElements(By.xpath(xpath));
-        List <String> result = new ArrayList <>();
-        for(WebElement e: elements){
-            String text = e.getText().replaceAll("\\d+(\\.\\d+)?", "");
-            text = text.replace("\n", " ");
-            result.add(text.trim());
-        }
-        return result;
-    }
+  public AbstractPage() {
+    driver = Driver.getInstance();
+    PageFactory.initElements(driver, this);
+  }
 }
